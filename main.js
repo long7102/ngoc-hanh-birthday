8a04f400659a76035dd2dc9538c373f6789ab635
Nguyễn Việt Long <105055082+long7102@users.noreply.github.com>
2022-10-06 10:45:28 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-confirm("Hãy chắc chắn rằng cô không bị sốc sau khi xem xong nhé");
 alert('Xin chào chị Ngọc Hạnh xinh đẹp, chúc chị có 1 ngày hạnh phúc và tốt lành. ');
 alert('Nhớ uống đủ 2 lít nước mỗi ngày nhé :)')
 alert('Nếu trang web quá to hãy nhấn tổ hợp Ctrl + - để nó nhỏ lại vừa xinh ');
 alert('Nếu trang web quá nhỏ thì nhấn Ctrl + + để nó to lên vừa đẹp ');
-setTimeout(function)({
-alert('Nhớ xem video 1 lần nữa trên youtube rồi đọc coment nha');
-},  30000)
+confirm("Trước khi tiếp tục, hãy chắc chắn rằng chị sẽ xem video thêm 1 lần nữa trên youtube và đọc coment");
