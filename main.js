249a5f6ddfa0b3b74eca25c23379e65bf0c5bba3
Nguyễn Việt Long <105055082+long7102@users.noreply.github.com>
2022-10-06 10:10:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-alert('Xin chao Ngoc Hanh, chuc ban co 1 ngay tot lanh va xinh dep. Nho uong du 2 lit nuoc moi ngay nhe :)')
+alert('Xin chào chị Ngọc Hạnh xinh đẹp, chúc chị có 1 ngày hạnh phúc và tốt lành. Nhớ uống đủ 2 lít nước mỗi ngày nhé :)')
