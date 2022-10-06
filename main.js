018bc40e9a886a0f4119a3502754e15764886ff3
Nguyễn Việt Long <105055082+long7102@users.noreply.github.com>
2022-10-06 10:41:01 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-confirm('Hãy chắc chắn rằng cô không bị sốc sau khi xem xong nhé'); 
+confirm("Hãy chắc chắn rằng cô không bị sốc sau khi xem xong nhé");
 alert('Xin chào chị Ngọc Hạnh xinh đẹp, chúc chị có 1 ngày hạnh phúc và tốt lành. ');
 alert('Nhớ uống đủ 2 lít nước mỗi ngày nhé :)')
 alert('Nếu trang web quá to hãy nhấn tổ hợp Ctrl + - để nó nhỏ lại vừa xinh ');
