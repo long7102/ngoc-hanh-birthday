ad76ae307585cad695bb8ed52b3434c42a038fc3
Nguyễn Việt Long <105055082+long7102@users.noreply.github.com>
2022-10-06 10:50:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-alert('Xin chào chị Ngọc Hạnh xinh đẹp, chúc chị có 1 ngày hạnh phúc và tốt lành. ');
-alert('Nhớ uống đủ 2 lít nước mỗi ngày nhé :)')
+alert('Xin chào chị Ngọc Hạnh xinh đẹp, chúc chị có 1 ngày hạnh phúc và tốt lành. (✿ ♡‿♡) ');
+alert('Nhớ uống đủ 2 lít nước mỗi ngày nhé (⌐□_□)');
 alert('Nếu trang web quá to hãy nhấn tổ hợp Ctrl + - để nó nhỏ lại vừa xinh ');
 alert('Nếu trang web quá nhỏ thì nhấn Ctrl + + để nó to lên vừa đẹp ');
-confirm("Trước khi tiếp tục, hãy chắc chắn rằng chị sẽ xem video thêm 1 lần nữa trên youtube và đọc coment");
+confirm("Trước khi tiếp tục, hãy chắc chắn rằng chị sẽ xem video thêm 1 lần nữa trên youtube và đọc comment giúp mình nha ◕‿↼, đi mà đi mà đi mà ");
